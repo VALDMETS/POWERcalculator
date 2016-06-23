a22318a379a71218aa0778371e74ef7c7c15e3c8
Ben Valdmets-Harris <benvaldmets@gmail.com>
2016-06-23 16:04:50 -0500

    works gooooood

--- main.js
+++ main.js
@@ -19,94 +19,140 @@ var clearbtn = document.getElementById('clear');
 
 var calculatebtn = document.getElementById('calculate');
 
+
 var num = [''];
 var counter = 0;
+var operate = [''];
+var oc = 0;
 
 var final = 0;
 
 onebtn.addEventListener('click', function(e) {
-  num[counter] = num[counter] + e.srcElement.value;
-  console.log(num[counter]);
+    num[counter] = num[counter] + e.srcElement.value;
+
+    console.log(num[counter]);
 });
 
 twobtn.addEventListener('click', function(e) {
-  num[counter] = num[counter] + e.srcElement.value;
-  console.log(num[counter]);
+    num[counter] = num[counter] + e.srcElement.value;
+    console.log(num[counter]);
 });
 
 threebtn.addEventListener('click', function(e) {
-  num[counter] = num[counter] + e.srcElement.value;
-  console.log(num[counter]);
+    num[counter] = num[counter] + e.srcElement.value;
+    console.log(num[counter]);
 });
 
 fourbtn.addEventListener('click', function(e) {
-  num[counter] = num[counter] + e.srcElement.value;
-  console.log(num[counter]);
+    num[counter] = num[counter] + e.srcElement.value;
+    console.log(num[counter]);
 });
 
 fivebtn.addEventListener('click', function(e) {
-  num[counter] = num[counter] + e.srcElement.value;
-  console.log(num[counter]);
+    num[counter] = num[counter] + e.srcElement.value;
+    console.log(num[counter]);
 });
 
 sixbtn.addEventListener('click', function(e) {
-  num[counter] = num[counter] + e.srcElement.value;
-  console.log(num[counter]);
+    num[counter] = num[counter] + e.srcElement.value;
+    console.log(num[counter]);
 });
 
 sevenbtn.addEventListener('click', function(e) {
-  num[counter] = num[counter] + e.srcElement.value;
-  console.log(num[counter]);
+    num[counter] = num[counter] + e.srcElement.value;
+    console.log(num[counter]);
 });
 
 eightbtn.addEventListener('click', function(e) {
-  num[counter] = num[counter] + e.srcElement.value;
-  console.log(num[counter]);
+    num[counter] = num[counter] + e.srcElement.value;
+    console.log(num[counter]);
 });
 
 ninebtn.addEventListener('click', function(e) {
-  num[counter] = num[counter] + e.srcElement.value;
-  console.log(num[counter]);
+    num[counter] = num[counter] + e.srcElement.value;
+    console.log(num[counter]);
 });
 
 zerobtn.addEventListener('click', function(e) {
-  num[counter] = num[counter] + e.srcElement.value;
-  console.log(num[counter]);
+    num[counter] = num[counter] + e.srcElement.value;
+    console.log(num[counter]);
+});
+pointbtn.addEventListener('click', function(e) {
+    num[counter] = num[counter] + e.srcElement.value;
+    console.log(num[counter]);
 });
 
 //clearout stuff!
 
-clearbtn.addEventListener('click', function(){
-  num[counter] = '';
-  clearOut ();
+clearbtn.addEventListener('click', function() {
+    num[counter] = '';
+    clearOut();
 });
 
 function clearOut() {
-  while (counter >= 0) {
-    num[counter] = '';
-    counter--;
-  }
-  counter++;
-  final = 0;
+    while (counter >= 0) {
+        num[counter] = '';
+        operate[oc] = 0;
+        counter--;
+        oc--;
+    }
+    counter++;
+    oc++;
+    final = 0;
 }
 
-
+//
 plusbtn.addEventListener('click', function() {
-  counter++;
-  num[counter] = '';
- });
+    oc++;
+    operate[oc] = 1;
+
+    counter++;
+    num[counter] = '';
+});
+minusbtn.addEventListener('click', function() {
+    oc++;
+    operate[oc] = 2;
+
+    counter++;
+    num[counter] = '';
+});
+multiplybtn.addEventListener('click', function() {
+    oc++;
+    operate[oc] = 3;
+
+    counter++;
+    num[counter] = '';
+});
+dividebtn.addEventListener('click', function() {
+    oc++;
+    operate[oc] = 4;
+
+    counter++;
+    num[counter] = '';
+});
 
 
 //calculations!
 
- calculate.addEventListener('click', function(){
-   for (cn = 0; cn<=counter; cn++){
-     num[cn]=Number(num[cn]);
-     final = final + num[cn];
-     num[cn]=String(num[cn]);
-   }
-   console.log(final);
-   clearOut();
-   num[counter] = String(num[counter]);
+calculate.addEventListener('click', function() {
+    for (i = 0; i <= oc; i++) {
+        num[i] = Number(num[i]);
+        if (i === 0) {
+            final = num[i];
+        } else
+        if (operate[i] === 1) {
+            final = final + num[i];
+        } else if (operate[i] === 2) {
+            final = final - num[i];
+        } else if (operate[i] === 3) {
+            final = final * num[i];
+        } else if (operate[i] === 4) {
+            final = final / num[i];
+        }
+    }
 
- });
+    console.log(final);
+    clearOut();
+    num[counter] = String(num[counter]);
+
+});
