6d889d17bd310138de23ce23d350d90dd72eb621
Ben Valdmets-Harris <benvaldmets@gmail.com>
2016-06-23 13:52:07 -0500

    calc takes string multi-numbers

---
+++ main.js
@@ -0,0 +1,64 @@
+var onebtn = document.getElementById('one');
+var twobtn = document.getElementById('two');
+var threebtn = document.getElementById('three');
+var fourbtn = document.getElementById('four');
+var fivebtn = document.getElementById('five');
+var sixbtn = document.getElementById('six');
+var sevenbtn = document.getElementById('seven');
+var eightbtn = document.getElementById('eight');
+var ninebtn = document.getElementById('nine');
+var zerobtn = document.getElementById('zero');
+var pointbtn = document.getElementById('point');
+var neg_posbtn = document.getElementById('neg-pos');
+
+var num1 = '';
+
+onebtn.addEventListener('click', function(e) {
+  num1 = num1 + e.srcElement.value;
+  console.log(num1);
+});
+
+twobtn.addEventListener('click', function(e) {
+  num1 = num1 + e.srcElement.value;
+  console.log(num1);
+});
+
+threebtn.addEventListener('click', function(e) {
+  num1 = num1 + e.srcElement.value;
+  console.log(num1);
+});
+
+fourbtn.addEventListener('click', function(e) {
+  num1 = num1 + e.srcElement.value;
+  console.log(num1);
+});
+
+fivebtn.addEventListener('click', function(e) {
+  num1 = num1 + e.srcElement.value;
+  console.log(num1);
+});
+
+sixbtn.addEventListener('click', function(e) {
+  num1 = num1 + e.srcElement.value;
+  console.log(num1);
+});
+
+sevenbtn.addEventListener('click', function(e) {
+  num1 = num1 + e.srcElement.value;
+  console.log(num1);
+});
+
+eightbtn.addEventListener('click', function(e) {
+  num1 = num1 + e.srcElement.value;
+  console.log(num1);
+});
+
+ninebtn.addEventListener('click', function(e) {
+  num1 = num1 + e.srcElement.value;
+  console.log(num1);
+});
+
+zerobtn.addEventListener('click', function(e) {
+  num1 = num1 + e.srcElement.value;
+  console.log(num1);
+});
