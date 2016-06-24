03173c7f12cc61569456463d8b230e92cd34e171
Ben Valdmets-Harris <benvaldmets@gmail.com>
2016-06-24 12:15:24 -0500

    styled and swanky

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ var eightbtn = document.getElementById('eight');
 var ninebtn = document.getElementById('nine');
 var zerobtn = document.getElementById('zero');
 var pointbtn = document.getElementById('point');
-var neg_posbtn = document.getElementById('neg-pos');
+var neg_posbtn = document.getElementById('negpos');
 
 var plusbtn = document.getElementById('plus');
 var minusbtn = document.getElementById('minus');
@@ -26,67 +26,43 @@ var operate = [''];
 var oc = 0;
 
 var final = 0;
+var evt = '';
 
-onebtn.addEventListener('click', function(e) {
-    num[counter] = num[counter] + e.srcElement.value;
+function buttonTime(e) {
+  num[counter] = num[counter] + e.srcElement.value;
+  // console.log(num[counter]);
+  document.getElementById('view').value = num[counter];
+}
 
-    console.log(num[counter]);
+onebtn.addEventListener('click', buttonTime);
+twobtn.addEventListener('click', buttonTime);
+threebtn.addEventListener('click', buttonTime);
+fourbtn.addEventListener('click', buttonTime);
+fivebtn.addEventListener('click', buttonTime);
+sixbtn.addEventListener('click', buttonTime);
+sevenbtn.addEventListener('click', buttonTime);
+eightbtn.addEventListener('click', buttonTime);
+ninebtn.addEventListener('click', buttonTime);
+zerobtn.addEventListener('click', buttonTime);
+pointbtn.addEventListener('click', buttonTime);
+
+neg_posbtn.addEventListener('click', function(e) {
+    var i = Number(num[counter]);
+    if (i>0) {
+        num[counter] = '-'+ num[counter];
+
+    } else {
+        num[counter] = String(num[counter]);
+        num[counter] = num[counter].replace('-','');
+    }
+    // console.log(num[counter]);
+    document.getElementById('view').value = num[counter];
 });
-
-twobtn.addEventListener('click', function(e) {
-    num[counter] = num[counter] + e.srcElement.value;
-    console.log(num[counter]);
-});
-
-threebtn.addEventListener('click', function(e) {
-    num[counter] = num[counter] + e.srcElement.value;
-    console.log(num[counter]);
-});
-
-fourbtn.addEventListener('click', function(e) {
-    num[counter] = num[counter] + e.srcElement.value;
-    console.log(num[counter]);
-});
-
-fivebtn.addEventListener('click', function(e) {
-    num[counter] = num[counter] + e.srcElement.value;
-    console.log(num[counter]);
-});
-
-sixbtn.addEventListener('click', function(e) {
-    num[counter] = num[counter] + e.srcElement.value;
-    console.log(num[counter]);
-});
-
-sevenbtn.addEventListener('click', function(e) {
-    num[counter] = num[counter] + e.srcElement.value;
-    console.log(num[counter]);
-});
-
-eightbtn.addEventListener('click', function(e) {
-    num[counter] = num[counter] + e.srcElement.value;
-    console.log(num[counter]);
-});
-
-ninebtn.addEventListener('click', function(e) {
-    num[counter] = num[counter] + e.srcElement.value;
-    console.log(num[counter]);
-});
-
-zerobtn.addEventListener('click', function(e) {
-    num[counter] = num[counter] + e.srcElement.value;
-    console.log(num[counter]);
-});
-pointbtn.addEventListener('click', function(e) {
-    num[counter] = num[counter] + e.srcElement.value;
-    console.log(num[counter]);
-});
-
-//clearout stuff!
 
 clearbtn.addEventListener('click', function() {
     num[counter] = '';
     clearOut();
+    document.getElementById('view').value = '';
 });
 
 function clearOut() {
@@ -101,38 +77,36 @@ function clearOut() {
     final = 0;
 }
 
-//
-plusbtn.addEventListener('click', function() {
-    oc++;
-    operate[oc] = 1;
+function nextNum() {
 
     counter++;
     num[counter] = '';
+}
+
+plusbtn.addEventListener('click', function() {
+    oc++;
+    operate[oc] = 1;
+    nextNum();
+    document.getElementById('view').value = '+' + num[counter];
 });
 minusbtn.addEventListener('click', function() {
     oc++;
     operate[oc] = 2;
-
-    counter++;
-    num[counter] = '';
+    nextNum();
+    document.getElementById('view').value = '-' + num[counter];
 });
 multiplybtn.addEventListener('click', function() {
     oc++;
     operate[oc] = 3;
-
-    counter++;
-    num[counter] = '';
+    nextNum();
+    document.getElementById('view').value = '*' + num[counter];
 });
 dividebtn.addEventListener('click', function() {
     oc++;
     operate[oc] = 4;
-
-    counter++;
-    num[counter] = '';
+    nextNum();
+    document.getElementById('view').value = '/' + num[counter];
 });
-
-
-//calculations!
 
 calculate.addEventListener('click', function() {
     for (i = 0; i <= oc; i++) {
@@ -150,9 +124,8 @@ calculate.addEventListener('click', function() {
             final = final / num[i];
         }
     }
-
-    console.log(final);
+    // console.log(final);
+    document.getElementById('view').value = final;
     clearOut();
     num[counter] = String(num[counter]);
-
 });
